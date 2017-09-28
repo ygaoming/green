26afe8eb0e0370bd73034bf632149747f134e658
苑高明 <1020876268@qq.com>
2017-09-28 18:24:02 +0800

    反馈php

---
+++ vagetables.php
@@ -0,0 +1,25 @@
+﻿<?php
+require_once "sql.phpHelper.class.php";
+header("Content-type: text/html; charset=utf-8");
+$sql = "select * from vagetables";
+$sqlHelper = new SqlHelper();
+$res = $sqlHelper->execute_dql($sql);
+while ($row = mysql_fetch_array($res,MYSQL_ASSOC)) {
+    $data[] = $row;
+}
+if(mysql_num_rows($res)>0){
+    $arr=[
+        'status'=>true,
+        'data'=>$data,
+        'msg'=>'获取成功'
+    ];
+    echo json_encode($arr);
+}else{
+    $arr=[
+        'status'=>true,
+        'data'=>[],
+        'msg'=>'没有数据'
+    ];
+    echo json_encode($arr);
+}
+$sqlHelper->close_connect();
---
+++ week.php
@@ -0,0 +1,29 @@
+﻿<?php
+require_once "sql.phpHelper.class.php";
+header("Content-type: text/html; charset=utf-8");
+$sql = "select * from week";
+$sqlHelper = new SqlHelper();
+$res = $sqlHelper->execute_dql($sql);
+while ($row = mysql_fetch_array($res,MYSQL_ASSOC)) {
+    
+    $img = explode(",", $row['img']);
+    
+    $row['img'] = $img;
+    $data[] = $row;
+}
+if(mysql_num_rows($res)>0){
+    $arr=[
+        'status'=>true,
+        'data'=>$data,
+        'msg'=>'获取成功'
+    ];
+    echo json_encode($arr);
+}else{
+    $arr=[
+        'status'=>true,
+        'data'=>[],
+        'msg'=>'没有数据'
+    ];
+    echo json_encode($arr);
+}
+$sqlHelper->close_connect();
